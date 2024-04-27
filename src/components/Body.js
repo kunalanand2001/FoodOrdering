e8dc31b86689b47ef81b64af094a7a0b873c4192
Kunal <kunalanand2001@gmail.com>
2024-04-27 10:58:10 +0530

    fetching live data from swiggy API and using useState hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,39 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-    const[listOfRestaurants, setlistOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setlistOfRestaurants] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const jsonData = await data.json();
+    console.log(jsonData);
+    setlistOfRestaurants(
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  };
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={() => {
+        <button
+          className="filter-btn"
+          onClick={() => {
             const filtededList = listOfRestaurants.filter(
-                (res)=> res.info.avgRating >= 4
+              (res) => res.info.avgRating >= 4
             );
             setlistOfRestaurants(filtededList);
-        }}>
+          }}
+        >
           Top Rated Restaurants
         </button>
       </div>
