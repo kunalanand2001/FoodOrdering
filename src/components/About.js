a1f9b63c7f75c92ca6267195b0cff8e87eed243c
Kunal <kunalanand2001@gmail.com>
2024-05-02 10:26:03 +0530

    made a basic ClassBasedComponent, UserClass

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,9 @@
+import UserClass from "./UserClass";
+
 const About = () =>{
     return(
         <div>
-            <h1>
-                This is About Page. 
-            </h1>
-            <h4>The description of the about page.</h4>
+            <UserClass name={"Kunal Kumar Anand"} location={"Dewas"}/>
         </div>
     )
 }
