4e075ee2f40a5cab683289bcfc8b78ae5acea9ce
Kunal <kunalanand2001@gmail.com>
2024-05-11 10:31:58 +0530

    did some UI changes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -13,15 +13,16 @@ const Cart = () => {
   };
 
   return (
-    <div className="text-center m-4 p-4 bg-gray-300">
+    <div className="text-center m-4 p-4 bg-gray-100 w-6/12 mx-auto rounded-xl">
       <h1 className="text-2xl font-bold">Cart</h1>
-      <button className="m-2 p-2 bg-red rounded-xl" onClick={handleClick}>
-        Clear Cart
-      </button>
-      <div className="w-6/12 mx-auto rounded-xl">
-        {cartItems.length === 0 && <h1>Cart Is Empty.</h1>}
+      <div>
+        {cartItems.length === 0 && <h1 className="font-bold italic mt-4 mb-2 p-2">Cart Is Empty.</h1>}
         <ItemList items={cartItems} />
       </div>
+
+      <button  className="m-4 px-4 py-2 bg-green-200 rounded-md" onClick={handleClick}>
+        Clear Cart
+      </button>
     </div>
   );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,12 @@
 const Contact = () => {
   return (
     <div>
-      <h1>This is Contact Us Page</h1>
-      <h4>The description of contact us page.</h4>
+      <h1 className="font-bold text-2xl m-2 p-2">This is Contact Us Page</h1>
+      <label className="ml-4 ">Enter EmailId : </label>
+      <input className="border m-2 p-2 bg-gray-200" />
+      <label>Enter Message : </label>
+      <input className="border m-2 p-2 bg-gray-200" />
+      <button className="m-4 px-4 py-2 bg-blue-200 rounded-md">submit</button>
     </div>
   );
 };
